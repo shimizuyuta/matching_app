57627b5e427e24b7a3c33d4f04f71b8bd0983c49
clean_water <shmizuyuta213@icloud.com>
2021-11-01 18:55:48 +0900

    frontend react routerセットアップ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route,Switch } from 'react-router-dom';
+import Register from './components/Register'
+import Login from './components/Login';
+import NotFound from './components/NotFound'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" render={(props)=><Register{...props}/>}/>
+        <Route exact path="/" component={Home}/>
+        <Route path="*" component={NotFound} />
+        <Route path="/notFound" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
