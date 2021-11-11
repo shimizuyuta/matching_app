4b8dc56b66459a3eaac25652b90165c0e7d00410
clean_water <shmizuyuta213@icloud.com>
2021-11-11 20:43:28 +0900

    urlに合致しないときの表示ページ

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React,{useEffect,useState,Fragment} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign:"center",
+    marginTop:theme.spacing(3),
+    margin:"auto",
+    padding:'auto',
+    fontSize:"3vw",
+  
+  },
+  title: {
+
+  },
+
+}));
 
 const NotFound = () => {
+  const classes = useStyles();
   return (
-    <div>
-      not found
-    </div>
+    <Container className={classes.root}>
+      <Typography variant="h1" className={classes.title}>404</Typography>
+      <Typography variant="h3" className={classes.title}>File Not Found</Typography>
+      <Typography variant="h4" className={classes.title}>アクセスしようとしたページは見つかりませんでした。</Typography>
+    </Container>
   )
 }
 
 export default NotFound
+
