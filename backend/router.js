2eb3bcdb9ef597ad833aa339aae3900661041759
clean_water <shmizuyuta213@icloud.com>
2021-11-11 21:33:49 +0900

    router設定

--- backend/router.js
+++ backend/router.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const AuthAPI = require('./API/controllers/AuthController')
+const UserAPI = require('./API/controllers/UserController')
+const verifyToken = require('./API/passport/strategies/jwtStrategy')
 
 router.post('/login',AuthAPI.login);
 router.post('/logout',AuthAPI.logout);
 router.post('/signup',AuthAPI.signup);
 
 
+router.get('/user',verifyToken,UserAPI.findOneUser)
+router.post('/user',verifyToken,UserAPI.updateUser)
+router.get('/users',verifyToken,UserAPI.findUser)
+
+
+router.get('/',(req,res)=>{
+  console.log(req);
+  res.status(200).json({})
+})
 
 module.exports = router;
