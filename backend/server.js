942fe6fda470c4b3993594fe1007db1b38bddeeb
clean_water <shmizuyuta213@icloud.com>
2021-11-12 12:05:05 +0900

    Heroku init

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const router = require('./router')
 const cors = require('cors')
 require('dotenv').config();
 const PORT = process.env.API_PORT || 3000
-
+const path = require('path')
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,8 +19,14 @@ initConnectionPool()
 .then((data)=>{
     console.log('mongo_data_connect_______________');
 })
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 require("./API/passport/config")(app);
+//本番用
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
+});
+//開発用
 app.use('/', router);
 
 app.listen(PORT,()=>console.log('backend server'))
